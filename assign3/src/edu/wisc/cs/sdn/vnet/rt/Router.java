588ae5260e51d9c5339699c2eb84cf58a009a3d4
HenryYihengXu <xu443@wisc.edu>
2019-10-29 10:59:09 -0500

    finished part 2

--- assign3/src/edu/wisc/cs/sdn/vnet/rt/Router.java
+++ assign3/src/edu/wisc/cs/sdn/vnet/rt/Router.java
@@ -93,6 +93,106 @@ public class Router extends Device {
 		/********************************************************************/
 	}
 
+	private void generateICMP(Ethernet originalPacket, Iface inIface) {
+		Ethernet ether = new Ethernet();
+		IPv4 ip = new IPv4();
+		ICMP icmp = new ICMP();
+		Data data = new Data();
+
+		IPv4 originalIPPacket = (IPv4) originalPacket.getPayload();
+
+		// is this the correct way to add padding?
+		short originalIPHeaderLen = originalIPPacket.getHeaderLength();
+		byte[] byteArr = new byte[12 + originalIPHeaderLen * 4];
+		for (int i = 0; i < 4; i++) {
+			byteArr[i] = 0;
+		}
+		// is this the correct way to append first 8 bytes following original IP Header?
+		byte[] originalIPPacketSerialized = originalIPPacket.serialize();
+		for (int i = 0; i < originalIPHeaderLen * 4 + 8; i++) {
+			byteArr[i + 4] = originalIPPacketSerialized[i];
+		}
+		data.setData(byteArr);
+
+		icmp.setIcmpType((byte) 11);
+		icmp.setIcmpCode((byte) 0);
+		icmp.resetChecksum();
+		icmp.serialize();
+
+		ip.setTtl((byte) 64);
+		ip.setProtocol(IPv4.PROTOCOL_ICMP);
+		ip.setSourceAddress(inIface.getIpAddress());
+		System.out.println("sourceIP: " + inIface.getIpAddress());
+		ip.setDestinationAddress(originalIPPacket.getSourceAddress());
+		System.out.println("destinationIP: " + originalIPPacket.getSourceAddress());
+		ip.resetChecksum();
+		ip.serialize();
+
+		ether.setEtherType(Ethernet.TYPE_IPv4);
+		// get source MAC from the interface where the original packet arrived
+		MACAddress sourceMAC = inIface.getMacAddress();
+		System.out.println("source MAC: " + sourceMAC);
+		ether.setSourceMACAddress(sourceMAC.toBytes());
+		// get destination MAC from source IP
+		// ether.setDestinationMACAddress(originalPacket.getSourceMACAddress());
+		//
+		int dstAddr = originalIPPacket.getSourceAddress();
+		RouteEntry bestMatch = this.routeTable.lookup(dstAddr);
+		int nextHop = bestMatch.getGatewayAddress();
+		if (0 == nextHop) {
+			nextHop = dstAddr;
+		}
+
+		// Set destination MAC address in Ethernet header
+		ArpEntry arpEntry = this.arpCache.lookup(nextHop);
+		if (null == arpEntry) {
+			return;
+		}
+		ether.setDestinationMACAddress(arpEntry.getMac().toBytes());
+		//
+		System.out.println("destination MAC: " + originalPacket.getSourceMAC());
+		System.out.println();
+
+		icmp.setPayload(data);
+		ip.setPayload(icmp);
+		ether.setPayload(ip);
+
+		sendPacket(ether, inIface);
+	}
+
+	private void generateARPReply(Ethernet etherPacket, Iface inIface) {
+
+		ARP arpPacket = (ARP) etherPacket.getPayload();
+		if (arpPacket.getOpCode() != ARP.OP_RARP_REQUEST) {
+			return;
+		}
+		int targetIp = ByteBuffer.wrap(arpPacket.getTargetProtocolAddress()).getInt();
+		if (targetIp != inIface.getIpAddress()) {
+			return;
+		}
+
+		Ethernet replyEther = new Ethernet();
+		ARP replyARP = new ARP();
+		replyEther.setPayload(replyARP);
+
+		replyEther.setEtherType(Ethernet.TYPE_ARP);
+		replyEther.setSourceMACAddress(inIface.getMacAddress().toBytes());
+		replyEther.setDestinationMACAddress(etherPacket.getSourceMACAddress());
+
+		replyARP.setHardwareType(ARP.HW_TYPE_ETHERNET);
+		replyARP.setProtocolType(ARP.PROTO_TYPE_IP);
+		replyARP.setHardwareAddressLength((byte) Ethernet.DATALAYER_ADDRESS_LENGTH);
+		replyARP.setProtocolAddressLength((byte) 4);
+		replyARP.setOpCode(ARP.OP_RARP_REPLY);
+		replyARP.setSenderHardwareAddress(inIface.getMacAddress().toBytes());
+		replyARP.setSenderProtocolAddress(inIface.getIpAddress());
+		replyARP.setTargetHardwareAddress(etherPacket.getSourceMACAddress());
+		replyARP.setTargetProtocolAddress(((IPv4) etherPacket.getPayload()).getSourceAddress());
+
+		sendPacket(replyEther, inIface);
+
+	}
+
 	private void handleIpPacket(Ethernet etherPacket, Iface inIface) {
 		// Make sure it's an IP packet
 		if (etherPacket.getEtherType() != Ethernet.TYPE_IPv4) {
@@ -114,9 +214,12 @@ public class Router extends Device {
 		}
 
 		// Check TTL
+		System.out.println("TTL before decrementing " + ipPacket.getTtl());
 		ipPacket.setTtl((byte) (ipPacket.getTtl() - 1));
 		// drop the packet if TTL is 0
 		if (0 == ipPacket.getTtl()) {
+			generateICMP(etherPacket, inIface);
+			// we might remove the return here.
 			return;
 		}
 
